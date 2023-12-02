cedb8eb640e47738d62fead92d6b20fe8df16dd3
Raul Batista <raulbatista94@gmail.com>
2023-12-02 11:45:20 +0100

    [chore] Add missing images

--- SharedUI/Sources/SharedUI/Sources/StyleGuide/Images/Image+AppImages.swift
+++ SharedUI/Sources/SharedUI/Sources/StyleGuide/Images/Image+AppImages.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 public extension Image {
+    // MARK: - Avatar
     static let avatarAlert = R.image.avatarAlert.image
     static let avatarAlien = R.image.avatarAlien.image
     static let avatarAngry = R.image.avatarAngry.image
@@ -34,4 +35,42 @@ public extension Image {
     static let avatarUnicorn = R.image.avatarUnicorn.image
     static let avatarWink = R.image.avatarWink.image
     static let defaultAvatar = R.image.defaultAvatar.image
+    
+    // MARK: - Common
+    static let background = R.image.background.image
+    static let currentPlayerBanner = R.image.currentPlayerBanner.image
+
+    // MARK: - Logo
+    static let logoLetterA = R.image.logoA.image
+    static let logoLetterI = R.image.logoI.image
+    static let logoLetterP = R.image.logoP.image
+    static let logoLetterR = R.image.logoR.image
+    static let logoLetterT = R.image.logoT.image
+    static let logoLetterTGreen = R.image.logoTGreen.image
+    static let logoLetterV = R.image.logoV.image
+    static let logoLetterYBlue = R.image.logoYBlue.image
+    static let logoLetterYYellow = R.image.logoYYellow.image
+    
+    // MARK: - Medal
+    static let medalBackground = R.image.bg_medal.image
+    static let medalGlow = R.image.glow.image
+    static let medalIcon = R.image.icon.image
+    static let medalContour = R.image.medal_contour.image
+    static let medal = R.image.medal.image
+    static let winnderRoundTitle = R.image.winnerTitle.image
+    
+    // MARK: - ProgressBars
+    static let progressBarRed = R.image.progressBar_1.image
+    static let progressBarPurple = R.image.progressBar_2.image
+    static let progressBarGreen = R.image.progressBar_3.image
+    static let progressBarYellow = R.image.progressBar_4.image
+    static let progressBarPink = R.image.progressBar_5.image
+    static let progressBarOrange = R.image.progressBar_6.image
+    static let progressBarBackground = R.image.progressBar_background.image
+    
+    // MARK: - Word swipe badge
+    static let wordSwipeArrow = R.image.arrow.image
+    static let wordTopShadow = R.image.topWordShadow.image
+    static let wordBadgeBackground = R.image.word.image
+    static let wordBadgeTopShadow = R.image.wordTopShadow.image
 }
--- SharedUI/Sources/SharedUI/Sources/StyleGuide/Images/UIImage+AppImages.swift
+++ SharedUI/Sources/SharedUI/Sources/StyleGuide/Images/UIImage+AppImages.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 public extension UIImage {
+    // MARK: - Avatar
     static let avatarAlert = R.image.avatarAlert()!
     static let avatarAlien = R.image.avatarAlien()!
     static let avatarAngry = R.image.avatarAngry()!
@@ -34,4 +35,42 @@ public extension UIImage {
     static let avatarUnicorn = R.image.avatarUnicorn()!
     static let avatarWink = R.image.avatarWink()!
     static let defaultAvatar = R.image.defaultAvatar()!
+
+    // MARK: - Common
+    static let background = R.image.background()!
+    static let currentPlayerBanner = R.image.currentPlayerBanner()!
+
+    // MARK: - Logo
+    static let logoLetterA = R.image.logoA()!
+    static let logoLetterI = R.image.logoI()!
+    static let logoLetterP = R.image.logoP()!
+    static let logoLetterR = R.image.logoR()!
+    static let logoLetterT = R.image.logoT()!
+    static let logoLetterTGreen = R.image.logoTGreen()!
+    static let logoLetterV = R.image.logoV()!
+    static let logoLetterYBlue = R.image.logoYBlue()!
+    static let logoLetterYYellow = R.image.logoYYellow()!
+    
+    // MARK: - Medal
+    static let medalBackground = R.image.bg_medal()!
+    static let medalGlow = R.image.glow()!
+    static let medalIcon = R.image.icon()!
+    static let medalContour = R.image.medal_contour()!
+    static let medal = R.image.medal()!
+    static let winnderRoundTitle = R.image.winnerTitle()!
+    
+    // MARK: - ProgressBars
+    static let progressBarRed = R.image.progressBar_1()!
+    static let progressBarPurple = R.image.progressBar_2()!
+    static let progressBarGreen = R.image.progressBar_3()!
+    static let progressBarYellow = R.image.progressBar_4()!
+    static let progressBarPink = R.image.progressBar_5()!
+    static let progressBarOrange = R.image.progressBar_6()!
+    static let progressBarBackground = R.image.progressBar_background()!
+    
+    // MARK: - Word swipe badge
+    static let wordSwipeArrow = R.image.arrow()!
+    static let wordTopShadow = R.image.topWordShadow()
+    static let wordBadgeBackground = R.image.word()!
+    static let wordBadgeTopShadow = R.image.wordTopShadow()!
 }
