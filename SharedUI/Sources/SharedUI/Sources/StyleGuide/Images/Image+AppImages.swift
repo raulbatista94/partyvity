c67437640e9a1db29726448bed59fe632031f025
Raul Batista <raulbatista94@gmail.com>
2023-12-02 11:17:22 +0100

    [chore] Add Images utility

---
+++ SharedUI/Sources/SharedUI/Sources/StyleGuide/Images/Image+AppImages.swift
@@ -0,0 +1,37 @@
+//
+//  File.swift
+//  
+//
+//  Created by Raul Batista on 02.12.2023.
+//
+
+import SwiftUI
+
+public extension Image {
+    static let avatarAlert = R.image.avatarAlert.image
+    static let avatarAlien = R.image.avatarAlien.image
+    static let avatarAngry = R.image.avatarAngry.image
+    static let avatarBackground = R.image.avatarBackground.image
+    static let avatarCash = R.image.avatarCash.image
+    static let avatarClaun = R.image.avatarClaun.image
+    static let avatarCuteSmile = R.image.avatarCuteSmile.image
+    static let avatarGeek = R.image.avatarGeek.image
+    static let avatarGhost = R.image.avatarGhost.image
+    static let avatarInLove = R.image.avatarInLove.image
+    static let avatarJapaneseSmile = R.image.avatarJapaneseSmile.image
+    static let avatarKiss = R.image.avatarKiss.image
+    static let avatarLOL = R.image.avatarLOL.image
+    static let avatarMonkey = R.image.avatarMonkey.image
+    static let avatarMonocle = R.image.avatarMonocle.image
+    static let avatarPoop = R.image.avatarPoop.image
+    static let avatarRanger = R.image.avatarRanger.image
+    static let avatarRobot = R.image.avatarRobot.image
+    static let avatarSkull = R.image.avatarSkull.image
+    static let avatarSleepy = R.image.avatarSleepy.image
+    static let avatarSmile = R.image.avatarSmile.image
+    static let avatarSmileNotSure = R.image.avatarSmileNotSure.image
+    static let avatarStarSmile = R.image.avatarStarSmile.image
+    static let avatarUnicorn = R.image.avatarUnicorn.image
+    static let avatarWink = R.image.avatarWink.image
+    static let defaultAvatar = R.image.defaultAvatar.image
+}
---
+++ SharedUI/Sources/SharedUI/Sources/StyleGuide/Images/RswiftResource.Image+Convenience.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Raul Batista on 02.12.2023.
+//
+
+import RswiftResources
+import SwiftUI
+
+extension RswiftResources.ImageResource {
+    var image: Image {
+        Image(self)
+    }
+}
