deee4dda6c5a11bd05da4e297096e630bb24ba99
Raul Batista <raulbatista94@gmail.com>
2024-01-09 08:02:45 +0100

    [chore] Fix crash with store url

--- Core/Sources/Common/Persistence/CoreData/URL+CoreData.swift
+++ Core/Sources/Common/Persistence/CoreData/URL+CoreData.swift
@@ -11,9 +11,12 @@ public extension URL {
     /// Returns a URL for the given app group and database pointing to the sqlite database.
     static func storeURL(for appGroup: String, databaseName: String, fileExtension: String = "sqlite") -> URL {
         let fileManager = FileManager.default
-        guard let fileContainer = fileManager.containerURL(forSecurityApplicationGroupIdentifier: appGroup) else {
+        guard
+            let fileContainer = fileManager.urls(for: .applicationSupportDirectory, in: .userDomainMask).last
+        else {
             fatalError("Shared file container could not be created.")
         }
+
         return fileContainer.appendingPathComponent("\(databaseName).\(fileExtension)")
     }
 }
